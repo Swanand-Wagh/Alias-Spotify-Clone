627c3854f1b930efe98b490ccda97d450ac87b28
Swanand-Wagh <swanandwagh7@gmail.com>
2021-01-26 00:43:28 +0530

    minor changes to the look of login page

--- src/Login.js
+++ src/Login.js
@@ -6,10 +6,7 @@ function Login() {
   return (
     <>
       <div className="login">
-        <img
-          src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-          alt=""
-        />
+        <img src={process.env.PUBLIC_URL + "/SpotifyLogo.svg"} alt="" />
         <a href={loginUrl}>LOGIN TO SPOTIFY CLONE</a>
       </div>
     </>
