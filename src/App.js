719470b5af04fb7af6ef9a51533bc3ea578c6390
Swanand-Wagh <swanandwagh7@gmail.com>
2021-02-08 22:03:23 +0530

    add useEffect to flush URL & load new after login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = ""; // clean the url
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
   return (
     <>
-      <Login />
+      <div>{token ? <h2>Logged in</h2> : <Login />}</div>
     </>
   );
 }
