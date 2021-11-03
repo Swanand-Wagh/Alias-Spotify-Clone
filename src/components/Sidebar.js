0692ace4471da4e45de69832ac211c1a28cddbd8
Swanand-Wagh <swanandwagh7@gmail.com>
2021-11-04 00:34:33 +0530

    first 3 SidebarOptions structure

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 const Sidebar = () => {
   return (
@@ -10,6 +14,9 @@ const Sidebar = () => {
           src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
           alt="sidebar_logo"
         />
+        <SidebarOption title="Home" Icon={HomeIcon} />
+        <SidebarOption title="Search" Icon={SearchIcon} />
+        <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
       </div>
     </>
   );
